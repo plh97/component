6eec6d0ff8efba11c91813b0bd97c6e594ef3aba
pengliheng <pengliheng111@gmail.com>
2018-01-24 18:22:20 +0800

    123

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import "../assets/common/common.less";
 import Modal from "./component/modal";
-import Table from "./component/table";
 import Message from "./component/message";
 import ModalInfo from "./component/modalInfo";
 import Spin from "./component/spin";
