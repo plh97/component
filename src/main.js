27a05b84366361c32f0634f997632b0ec6507807
pengliheng <pengliheng111@gmail.com>
2018-05-08 22:02:15 +0800

    32

--- src/main.js
+++ src/main.js
@@ -4,44 +4,44 @@
 import '../assets/common/common.less';
 import Modal from './component/modal';
 import Message from './component/message';
-import ModalInfo from './component/modalInfo';
-import Spin from './component/spin';
-import Icon from './container/icon';
-// import scrollBind from './utils/scrollBind';
-import treeTablePC from './component/treeTable/pc';
-import treeTableMobile from './component/treeTable/mobile';
-import tablePC from './component/table/pc';
-import treePC from './component/tree/pc';
-import treeMobile from './component/tree/mobile';
-import Button from './container/button';
-import Dom from './utils/dom';
+// import ModalInfo from './component/modalInfo';
+// import Spin from './component/spin';
+// import Icon from './container/icon';
+// // import scrollBind from './utils/scrollBind';
+// import treeTablePC from './component/treeTable/pc';
+// import treeTableMobile from './component/treeTable/mobile';
+// import tablePC from './component/table/pc';
+// import treePC from './component/tree/pc';
+// import treeMobile from './component/tree/mobile';
+// import Button from './container/button';
+// import Dom from './utils/dom';
 
-const { isMobile } = Dom;
+// const { isMobile } = Dom;
 
 
 const Component = {
-  Message,
-  Spin,
-  Modal,
-  ModalInfo,
-  Button,
-  Icon,
+  // Message,
+  // Spin,
+  // Modal,
+  // ModalInfo,
+  // Button,
+  // Icon,
   // scrollBind,
-  pc: {
-    message: Message,
-    spin: Spin,
-    modal: Modal,
-    table: isMobile() ? treeMobile : tablePC,
-    tree: isMobile() ? treeMobile : treePC,
-    treeTable: isMobile() ? treeTableMobile : treeTablePC,
-    // modalInfo: ModalInfo,
-  },
-  mobile: {
-    spin: Spin,
-    modal: Modal,
-    message: Message,
-    modalInfo: ModalInfo,
-  }
+  // pc: {
+  message: Message,
+  // spin: Spin,
+  modal: Modal,
+  // table: isMobile() ? treeMobile : tablePC,
+  // tree: isMobile() ? treeMobile : treePC,
+  // treeTable: isMobile() ? treeTableMobile : treeTablePC,
+  // modalInfo: ModalInfo,
+  // },
+  // mobile: {
+  //   spin: Spin,
+  //   modal: Modal,
+  //   message: Message,
+  //   modalInfo: ModalInfo,
+  // },
 };
 
 window.Component = Component;
